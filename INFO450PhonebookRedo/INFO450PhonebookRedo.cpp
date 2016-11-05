a2cb4fa56baea32e51c26d73247dd907bb6ac093
Harry <kalrah2@vcu.edu>
2016-11-04 22:34:10 -0400

    Did the best I could

--- INFO450PhonebookRedo/INFO450PhonebookRedo.cpp
+++ INFO450PhonebookRedo/INFO450PhonebookRedo.cpp
@@ -1,23 +1,25 @@
 // INFO450PhonebookRedo.cpp : Defines the entry point for the console application.
 //Harry Kalra
 
+//could not get the duplicate check to work
+
 #include "stdafx.h"
 #include <iostream>
 #include <stdio.h>
 #include <fstream>
 using namespace std;
 
-class phoneBook
+class phoneBook 
 {
-	char fullName[25];
-	char homeNumber[10];
-	char mobileNumber[10];
+	char fullName[25]; //first name and last name
+	char homeNumber[25]; //home phone number
+	char mobileNumber[25];// mobile phone number
 
 public:
 	phoneBook();
 	void enterphoneBook(char fn[], char hn[], char mn[]);
 	void display();
-	friend class numberList;
+	friend class numberList; //friend class
 
 };
 
@@ -39,21 +41,21 @@ void phoneBook::enterphoneBook(char fn[], char hn[], char mn[])
 
 void phoneBook::display()
 {
-	cout << " Please Enter Name" << fullName << endl;
-	cout << "Please enter Home Phone Number" << homeNumber << endl;
-	cout << "Please enter Mobile Phone Number" << mobileNumber << endl;
+	cout << "Name:" << fullName << endl;
+	cout << "Home Phone Number:" << homeNumber << endl;
+	cout << "Mobile Phone Number:" << mobileNumber << endl;
 }
 
 class numberList
 {
 	char filename[100];
+	int entryCount;
 	phoneBook **contacts;
 
 public:
 	numberList();
 	~numberList();
 	void setfilename(char f[]);
-	int entryCount;
 	int getCount();
 	void addToList(phoneBook *p);
 	void shownameList();
@@ -90,6 +92,19 @@ void numberList::addToList(phoneBook * p)
 
 void numberList::shownameList()
 {
+	for (int i = 0; i < entryCount; i++)
+	{
+		cout << "Record:" << i + 1 << endl;
+		contacts[i]->display();
+
+		if ((i + 1) % 5 == 0)
+		{
+			char answer;
+			cout << "Press any key to continue" << endl;
+			cin >> answer;
+			cin.ignore();
+		}
+	}
 }
 
 int numberList::savenameList()
@@ -141,8 +156,9 @@ int main()
 {
 	char answer = 'y';
 	char fullName[25];
-	char homeNumber[10];
-	char mobileNumber[10];
+	char homeNumber[25];
+	char mobileNumber[25];
+	char filename[50];
 
 	numberList *bList = new numberList();
 
@@ -151,41 +167,40 @@ int main()
 	bList->setfilename(filename);
 
 	bList->readnameList();
-	if (bList->getCount() == 0)
+	if (bList->getCount() == 0 || bList->getCount() < 100)
 	{
 		cout << "Your Phonebook has no names." << endl;
 	}
 	else
 	{
-		cout << "There are" << bList->getCount() << "numbers in your PhoneBook" << endl
+		cout << "There are" << bList->getCount() << "numbers in your PhoneBook" << endl;
 	}
-	cout << "Would you like to add a number? Please enter Y to add an entry" << endl;
+	cout << "Would you like to add a number? Please enter Y to add an entry. Press E to Exit" << endl;
 	cin >> answer;
 
 	while (answer == 'Y' || answer == 'y')
 	{
 		cin.ignore();
 		cin.clear();
+		
+			cout << "Enter the Full Name" << endl;
+			gets_s(fullName);
+			cout << "Enter the Home Phone Number" << endl;
+			gets_s(homeNumber);
+			cout << "Enter the Mobile Number" << endl;
+			gets_s(mobileNumber);
 
-		cout << "Enter the Full Name" << endl;
-		gets_s(fullName);
-		cout << "Enter the Home Phone Number" << endl;
-		gets_s(homeNumber);
-		cout << "Enter the Mobile Number" << endl;
-		gets_s(mobileNumber);
-
-		phoneBook *myphoneBook = new phoneBook();
-		myphoneBook->enterphoneBook(fullName, homeNumber, mobileNumber);
-		bList->addToList(myphoneBook);
-
-
-		cout << "Would you like to enter number?" << endl;
-		cout << "Y for Yes." << endl;
-		cin >> answer;
-	}
+			phoneBook *myphoneBook = new phoneBook();
+			myphoneBook->enterphoneBook(fullName, homeNumber, mobileNumber);
+			bList->addToList(myphoneBook);
+	
+			cout << "Would you like to add a number? Please enter Y to add an entry. Press E to Exit" << endl;
+			cin >> answer;
+		}
 
 	bList->shownameList();
 	bList->savenameList();
 
 	return 0;
 }
+
